f2b628f4cbe8a2e44dfc2173f76e93cb06da107b
Teelo <49792789+teelolws@users.noreply.github.com>
2023-04-10 09:07:20 +1200

    Add the expansion name "factions" that are showing as a reputation bar when all their subfactions are marked inactive

--- Source/InactiveReputationHeaders.lua
+++ Source/InactiveReputationHeaders.lua
@@ -4,7 +4,9 @@ if not IRH_DB or (type(IRH_DB) ~= "table") then
     IRH_DB = {}
 end
 
-local reputationsToOverride = {[67] = true, [469] = true, [1037] = true, [1052] = true, [1272] = true, [1302] = true, [2445] = true, [2510] = true,}
+local reputationsToOverride = {[67] = true, [469] = true, [1037] = true, [1052] = true, [1272] = true, [1302] = true, [2445] = true, [2510] = true,
+    [1162] = true, [1834] = true, [980] = true, [1444] = true, -- these 4 are "Cataclysm", "Legion" etc, they only appear if all their subfactions are marked inactive, and... not sure why they're showing as a reputation at all. Probably a bug. Have been showing since Dragonflight launch.
+}
 
 -- simple map: original index => new index
 local reputations = {}
@@ -31,52 +33,41 @@ local function compileNewTable()
     
     -- remove reputations flagged overwrite inactive
     local removed = {}
-    local function recursion(k)
-        for i = k, numFactions do
-            if IRH_DB[select(14, originalGetFactionInfo(reputations[i]))] then
-                local name = originalGetFactionInfo(reputations[i])
-                table.insert(removed, reputations[i])
+    for i = numFactions, 1, -1 do
+        if IRH_DB[select(14, originalGetFactionInfo(reputations[i]))] then
+            local name = originalGetFactionInfo(reputations[i])
+            table.insert(removed, reputations[i])
+            for j = i, numFactions do
+                reputations[j] = reputations[j+1]
+            end
+            reputations[numFactions] = nil
+            numFactions = numFactions - 1
+        end
+    end
+    
+    -- remove headers that have no reputations in them anymore
+    for i = numFactions, 1, -1 do
+        local name, _, _, _, _, _, _, _, isHeader, isCollapsed, _, _, isChild = originalGetFactionInfo(reputations[i])
+        if (not (name == FACTION_INACTIVE)) and isHeader and (not isCollapsed) and (not isChild) then
+            local containsOther = false
+            local _, _, _, _, _, _, _, _, isHeader2, _, _, _, isChild2 = originalGetFactionInfo(reputations[i+1])
+            if isHeader2 and (not isChild2) then
                 for j = i, numFactions do
                     reputations[j] = reputations[j+1]
                 end
                 reputations[numFactions] = nil
                 numFactions = numFactions - 1
-                recursion(i)
-                return
             end
         end
     end
-    recursion(1)
-    
-    -- remove headers that have no reputations in them anymore
-    function recursion(k)
-        for i = k, numFactions do
-            local name, _, _, _, _, _, _, _, isHeader, isCollapsed, _, _, isChild = originalGetFactionInfo(reputations[i])
-            if (not (name == FACTION_INACTIVE)) and isHeader and (not isCollapsed) and (not isChild) then
-                local containsOther = false
-                local _, _, _, _, _, _, _, _, isHeader2, _, _, _, isChild2 = originalGetFactionInfo(reputations[i+1])
-                if isHeader2 and (not isChild2) then
-                    -- skip this header, too
-                    for j = i, numFactions do
-                        reputations[j] = reputations[j+1]
-                    end
-                    reputations[numFactions] = nil
-                    numFactions = numFactions - 1
-                    recursion(i)
-                    return
-                end
-            end
-        end
-    end
-    recursion(1)
             
     -- find the faction index of the "Inactive" header
     local inactiveCategoryFactionIndex = 0
     for i = 1, originalGetNumFactions() do
         local name = originalGetFactionInfo(i)
         if name == FACTION_INACTIVE then
-            inactiveCategoryFactionIndex = i
             if select(10, originalGetFactionInfo(i)) then return end -- if Inactive is collapsed, no need to inject removed factions
+            inactiveCategoryFactionIndex = i
             break
         end
     end
@@ -140,13 +131,14 @@ ReputationDetailInactiveCheckBox:HookScript("OnClick", function(self)
     local factionID = select(14, GetFactionInfo(factionIndex))
 	if reputationsToOverride[factionID] then 
         if not self:GetChecked() then
-		    PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON);
+            PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON);
             IRH_DB[factionID] = true
-            C_Timer.After(0.1, ReputationFrame_Update)
         else
-		    PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_OFF);
+            PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_OFF);
             IRH_DB[factionID] = nil
         end
+        lastReputationFrameUpdate = 0
+        lastCompile = 0
         ReputationFrame_Update()
     end
 end)
@@ -154,15 +146,13 @@ end)
 local originalExpandFactionHeader = ExpandFactionHeader
 function ExpandFactionHeader(index)
     originalExpandFactionHeader(reputations[index])
-    compileNewTable()
-    C_Timer.After(0.1, ReputationFrame_Update)
+    ReputationFrame_Update()
 end
 
 local originalCollapseFactionHeader = CollapseFactionHeader
 function CollapseFactionHeader(index)
     originalCollapseFactionHeader(reputations[index])
-    compileNewTable()
-    C_Timer.After(0.1, ReputationFrame_Update)
+    ReputationFrame_Update()
 end
 
 local originalFactionToggleAtWar = FactionToggleAtWar
@@ -173,15 +163,13 @@ end
 local originalSetFactionActive = SetFactionActive
 function SetFactionActive(index)
     originalSetFactionActive(reputations[index])
-    compileNewTable()
-    C_Timer.After(0.1, ReputationFrame_Update)
+    ReputationFrame_Update()
 end
 
 local originalSetFactionInactive = SetFactionInactive
 function SetFactionInactive(index)
     originalSetFactionInactive(reputations[index])
-    compileNewTable()
-    C_Timer.After(0.1, ReputationFrame_Update)
+    ReputationFrame_Update()
 end
 
 local originalSetWatchedFactionIndex = SetWatchedFactionIndex
