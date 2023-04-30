62f24e4de6c71e349e94786eaed2fbfd2a3b75d3
Teelo <49792789+teelolws@users.noreply.github.com>
2023-04-30 18:35:18 +1200

    gitignore

--- mklink.bat
+++ mklink.bat
@@ -1,1 +1,1 @@
-mklink /j /D "C:\Users\teelo\Documents\GitHub\InactiveReputationHeaders\Source" "F:\World of Warcraft\_retail_\Interface\AddOns\InactiveReputationHeaders"
+mklink /j /D "C:\Users\teelo\Documents\GitHub\InactiveReputationHeaders\Source" "F:\World of Warcraft\_ptr_\Interface\AddOns\InactiveReputationHeaders"
