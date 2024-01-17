ae9540400db9a303519c8ad6028d7d28868e3316
Teelo <49792789+teelolws@users.noreply.github.com>
2024-01-17 18:45:38 +1300

    Add Dragonscale Expedition
    It is now a header for the new archaeology reputation

--- Source/InactiveReputationHeaders.lua
+++ Source/InactiveReputationHeaders.lua
@@ -2,7 +2,7 @@ local addonName, addon = ...
 
 -- Replaces the functions in FrameXML\ReputationFrame.lua with a variant that allows reputation headers to be marked inactive
 
-addon.reputationsToOverride = {[67] = true, [469] = true, [1037] = true, [1052] = true, [1272] = true, [1302] = true, [2445] = true, [2510] = true,
+addon.reputationsToOverride = {[67] = true, [469] = true, [1037] = true, [1052] = true, [1272] = true, [1302] = true, [2445] = true, [2507] = true, [2510] = true,
     [1162] = true, [1834] = true, [980] = true, [1444] = true, -- these 4 are "Cataclysm", "Legion" etc, they only appear if all their subfactions are marked inactive, and... not sure why they're showing as a reputation at all. Probably a bug. Have been showing since Dragonflight launch.
 }
 
