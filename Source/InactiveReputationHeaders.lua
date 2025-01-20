bafea75d03e831c75695b50c1d3f4bdb0747cf51
Teelo <49792789+teelolws@users.noreply.github.com>
2025-01-20 21:45:59 +1300

    Fix issue with expansions being shown as reputations if all their sub-reps are hidden
    Closes: https://github.com/teelolws/InactiveReputationHeaders/issues/16

--- Source/InactiveReputationHeaders.lua
+++ Source/InactiveReputationHeaders.lua
@@ -13,6 +13,8 @@ addon.reputationsToOverride = {
     [2507] = true,
     [2510] = true,
     [2600] = true,
+    
+    [1162] = true, [1834] = true, [980] = true, [1444] = true, [2104] = true, [2414] = true, [2506] = true, [2569] = true, -- these 4 are "Cataclysm", "Legion" etc, they only appear if all their subfactions are marked inactive, and... not sure why they're showing as a reputation at all. Probably a bug. Have been showing since Dragonflight launch.
 }
 
 EventUtil.ContinueOnAddOnLoaded(addonName, function()
