f601f66a7ac9495db7137a5b3c334ffdf590974c
Teelo <49792789+teelolws@users.noreply.github.com>
2025-01-20 16:15:25 +1300

    Delete plunderstorm-renown.lua

--- Source/plunderstorm-renown.lua
+++
@@ -1,8 +0,0 @@
--- Make the Plunderstorm faction's Renown button usable
-
-hooksecurefunc(ReputationFrame.ReputationDetailFrame.ViewRenownButton, "Refresh", function(self)
-    local factionID = C_Reputation.GetFactionDataByIndex(C_Reputation.GetSelectedFaction()).factionID
-    if factionID == 2593 then
-        self:Enable()
-    end
-end)
